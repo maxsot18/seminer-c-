c8bed89feddb75ac9b5815cbcecccb50d5b3c46d
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-03 00:44:28 -0800

    Task01_ofHW

---
+++ Seminar2_task01/Program.cs
@@ -0,0 +1,26 @@
+﻿// // Задача 1: Напишите программу, 
+// которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа. 
+// Не использовать строки
+
+// // 456 -> 5
+// // 782 -> 8
+
+int Prompt(string message)
+{
+    Console.Write($"{message} ");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+int number = Prompt("Number = ");
+
+
+if (number >= 100 & number < 1000)
+{
+    double number1 = number % 100;
+    int number2 = Convert.ToInt32(number1);
+    int number3 = number2 / 10;
+    Console.WriteLine($"{number3}");
+}
+else { Console.WriteLine("Not ThreeDigitNumber"); }
+
