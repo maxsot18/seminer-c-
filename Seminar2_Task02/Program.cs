57bc4bafa5b716f87697cde5b020e29ab49c5eed
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-03 01:02:53 -0800

    Task02_ofHW

---
+++ Seminar2_Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Напишите программу, 
+// которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа. 
+// Не использовать строки для расчета
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int Prompt(string message)
+{
+    Console.Write($"{message} ");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+int number = Prompt("Number = ");
+
+if (number >= 100 & number < 1000)
+{
+    int number1 = number / 100;
+    double number2 = number % 10;
+    int number3 = Convert.ToInt32(number2);
+    Console.Write($"{number1}");
+    Console.Write($"{number3}");
+
+}
+else { Console.WriteLine("Not ThreeDigitNumber"); }
