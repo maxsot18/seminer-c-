e12822cf5ec2bf67adb6cf334cc6ed418e863b94
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-04 04:33:28 -0800

    Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 5*: 
+// Напишите программу, которая генерирует 
+// последовательность случайных чисел из 10 элементов в 
+// диапазоне от 1 до 10, и подсчитывает, 
+// сколько сгенерировалось чисел больше 5.
+
+int[] array = new int[10];
+int morefive = 0;
+//  int tmp = new Random().Next(1, 10);
+for (int i = 0; i < array.Length; i++)
+{
+    
+    int tmp = new Random().Next(1, 10);
+     if (tmp > 5)
+     {
+        morefive = morefive + 1;
+        
+        
+     }
+
+    
+}
+Console.WriteLine($"Numbers > 5 = {morefive}");
